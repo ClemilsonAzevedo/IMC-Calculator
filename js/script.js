1ff613ec5da28a0859bc8384c75a917533aad937
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-08-06 17:12:42 +0100

    validando Dados

--- js/script.js
+++ js/script.js
@@ -9,14 +9,27 @@ const inputHeight = document.querySelector('#height')
 form.onsubmit = (event) => {
   event.preventDefault()
 
+  document.querySelector('.alert-error').classList.remove('open')
+
   const weight = inputWeight.value
   const height = inputHeight.value
+
+  const showAlertError = notANumber(weight) || notANumber(height)
+
+  if (showAlertError) {
+    document.querySelector('.alert-error').classList.add('open')
+    return
+  }
 
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
   
   Modal.message.innerText = message
   Modal.open()
+}
+
+function notANumber (value){
+  return isNaN(value) ||value == ""
 }
 
 function IMC(weight, height){
