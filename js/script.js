78cc04faaf9c37ee800dbf8ab7a1c9fa18bf4ec7
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-08-06 16:51:23 +0100

    Modularizando o Modal

--- js/script.js
+++ js/script.js
@@ -1,35 +1,23 @@
+import { Modal } from './modal.js'
+
+
 // Variaveis
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-
-const modal = {
-  wrapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .title span'),
-  buttonClose: document.querySelector('.modal button.close'),
-  open(){
-    this.wrapper.classList.add('open')
-  },
-  close(){
-    this.wrapper.classList.remove('open')
-  }
-}
 
 form.onsubmit = (event) => {
   event.preventDefault()
 
   const weight = inputWeight.value
   const height = inputHeight.value
-  
+
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
   
-  modal.message.innerText = message
-  modal.open()
+  Modal.message.innerText = message
+  Modal.open()
 }
-
-modal.buttonClose.onclick = () => modal.close()
-
 
 function IMC(weight, height){
   return (weight / ((height / 100) ** 2)).toFixed(2)
